b74895dcf229530347a65e18bf0d0e21f3dd7026
amit-sharma9 <amitsharma83605@gmail.com>
2024-03-24 21:01:39 +0530

    updated the path

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("hi i am the root ");
+  res.redirect("/listings");
 });
 
 // ------------------------------
